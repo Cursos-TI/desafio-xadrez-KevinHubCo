405d5859f9f24ca6109a9ae90616cdb16a6f68a4
KevinHubCo <kevincorreia2002@gmail.com>
2026-05-11 18:13:16 +0000

    Substitui os loops da torre, bispo, rainha e cavalo para funções recursividade, para torre e rainha usei o if por ser um movimento simples, mas pro bispo e cavalo usei estruturas aninhadas com for e no cavalo usei o continue e break

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,6 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
@@ -28,41 +27,66 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
     // ENTRADA DOS DADOS
+
+void movtorre(int num){
+    if (num > 0){
+        printf(" - Direita \n");
+        movtorre(num - 1);
+    }
+}
+void movbispo(int num){
+    if (num > 0) {
+        for (int i = 0; i < 1; i++) {
+            printf(" - Cima, ");
+            for ( int j = 0; j < 1; j++){
+                printf("Direita\n");
+            }
+            movbispo (num - 1);
+        }
+    }
+}
+void movrainha(int num){
+    if (num > 0){
+        printf(" - Esquerda\n");
+        movrainha(num - 1);
+    }
+}
+
+void movcavalo(int num){
+    for (int i = 0; i < num; i++) {
+        for (int j = 1; j <= 3; j++){
+            if (j <= 2){
+                printf(" - Cima\n");
+                continue;
+            }
+        printf(" - Direita\n");
+        break;
+        }
+    }
+}
+
+int main () {
     int casastorre = 5;
-    int casabispo = 5;
-    int casarainha = 8;
+    int casasbispo = 5;
+    int casasrainha = 8;
+    int contadorcavalo = 1;
 
     printf("===== MOVIMENTO DA TORRE ======\n");
-    // MOVENDO PARA DIREITA USANDO O LOOP FOR
-    for (int i = 0; i < casastorre; i++) {
-        printf("Direita\n");
-    }
+    // MOVENDO PARA DIREITA USANDO A RECURSIVIDADE
+    movtorre(casastorre);
 
     printf("====== MOVIMENTO DO BISPO ======\n");
-    //MOVENDO NA DIAGONAL (CIMA E DIREITA) USANDO LOOP WHILE
-    int contadorbispo = 0;
-    while (contadorbispo < casabispo){
-        printf("Cima, Direita\n");
-        contadorbispo++;
-    }
-    //MOVENDO A RAINHA PARA A ESQUERDA USANDO O LOOP DO-WHILE
-    printf("====== MOVIMENTO DA RAINHA ======\n");
-    int contadorrainha = 0;
-    do {
-        printf("Esquerda\n");
-        contadorrainha++;
-    }while (contadorrainha < casarainha);
+    //MOVENDO NA DIAGONAL (CIMA E DIREITA) USANDO RECURSIVIDADE E FOR ANINHADO
+    movbispo(casasbispo);
 
-    //MOVENDO O CAVALO DUAS VEZES PARA BAIXO E UMA PRA ESQUERDA FOR / WHILE
+    printf("====== MOVIMENTO DA RAINHA ======\n");
+    //MOVENDO A RAINHA PARA A ESQUERDA USANDO RECURSIVIDADE
+    movrainha(casasrainha);
+
     printf("====== MOVIMENTO DO CAVALO ======\n");
-    int contadorcavalo = 0;
-    for (int i = 1; i <= 2; i++) {
-        printf("Baixo\n");
-    }
-    while (contadorcavalo < 1){
-        printf("Esquerda\n");
-        contadorcavalo++;
-    }
-    
+    //MOVENDO O CAVALO EM FORMATO DE "L", USANDO RECURSIVIDADE, FOR E CONTINUE E BREAK
+    movcavalo(contadorcavalo);
+
+
     return 0;
 }
